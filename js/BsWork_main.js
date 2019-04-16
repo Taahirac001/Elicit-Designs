9dbc867472afd8868e6757c181a5a49625441054
Taahirac001 <49667439+Taahirac001@users.noreply.github.com>
2019-04-16 00:15:01 -0400

    Add files via upload

---
+++ js/BsWork_main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.client1').hover(function(){
+        $('.infobox1').toggle(function(){
+            $(this).show();
+        })
+    })
+
+    $('.client2').hover(function(){
+        $('.infobox2').toggle(function(){
+            $(this).show();
+        })
+    })
+    
+    $('.client3').hover(function(){
+        $('.infobox3').toggle(function(){
+            $(this).show();
+        })
+    })
+    
+  
+    
+})
